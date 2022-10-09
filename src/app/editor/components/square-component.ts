b623f2b775cacaa5a9597c2573f4b7e83f0978bb
Doesn'tmeananything <andrey.krasnov.pm@gmail.com>
2022-10-09 18:14:16 +0300

    Add square component

---
+++ src/app/editor/components/square-component.ts
@@ -0,0 +1,59 @@
+// @ts-nocheck
+import { Component, Input, Output } from 'rete';
+
+import { numSocket } from '../sockets';
+import { NumControl } from '../controls/number-control';
+
+export class SquareComponent extends Component {
+  override data: any;
+  constructor() {
+    super('Square');
+  }
+
+  async builder(node: {
+    addInput: (arg0: Input) => {
+      (): any;
+      new (): any;
+      addInput: {
+        (arg0: Input): {
+          (): any;
+          new (): any;
+          addControl: {
+            (arg0: NumControl): {
+              (): any;
+              new (): any;
+              addOutput: { (arg0: Output): void; new (): any };
+            };
+            new (): any;
+          };
+        };
+        new (): any;
+      };
+    };
+  }) {
+    const inp = new Input('num1', 'Number', numSocket);
+    const out = new Output('num', 'Number', numSocket);
+
+    inp.addControl(new NumControl(this.editor, 'num1'));
+
+    node
+      .addInput(inp)
+      .addControl(new NumControl(this.editor, 'preview', true))
+      .addOutput(out);
+  }
+
+  worker(
+    node: { data: { num1: any }; id: number },
+    inputs: { [x: string]: any[] },
+    outputs: { [x: string]: any }
+  ) {
+    const n1 = inputs['num1'].length ? inputs['num1'][0] : node.data.num1;
+    const square = n1 * n1;
+
+    const ctrl = this.editor.nodes
+      .find((n) => n.id === node.id)
+      .controls.get('preview') as NumControl;
+    ctrl.setValue(square);
+    outputs['num'] = square;
+  }
+}
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -2,13 +2,14 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { NodeEditor, Engine } from 'rete';
 
+import { AngularRenderPlugin } from 'rete-angular-render-plugin';
 import AreaPlugin from 'rete-area-plugin';
 import ConnectionPlugin from 'rete-connection-plugin';
 import ContextMenuPlugin from 'rete-context-menu-plugin';
 
 import { NumComponent } from './components/number-component';
 import { AddComponent } from './components/add-component';
-import { AngularRenderPlugin } from 'rete-angular-render-plugin';
+import { SquareComponent } from './components/square-component';
 
 @Component({
   selector: 'app-editor',
@@ -39,7 +40,11 @@ export class EditorComponent implements AfterViewInit {
   async ngAfterViewInit() {
     const container = this.el.nativeElement;
 
-    const components = [new NumComponent(), new AddComponent()];
+    const components = [
+      new NumComponent(),
+      new AddComponent(),
+      new SquareComponent(),
+    ];
 
     const editor = new NodeEditor('demo@0.2.0', container);
     editor.use(ConnectionPlugin);
@@ -51,21 +56,23 @@ export class EditorComponent implements AfterViewInit {
 
     components.map((c) => {
       editor.register(c);
-
       engine.register(c);
     });
 
     const n1 = await components[0].createNode({ num: 2 });
     const n2 = await components[0].createNode({ num: 0 });
     const add = await components[1].createNode();
+    const square = await components[2].createNode();
 
     n1.position = [80, 200];
     n2.position = [80, 400];
-    add.position = [500, 240];
+    add.position = [400, 240];
+    square.position = [700, 240];
 
     editor.addNode(n1);
     editor.addNode(n2);
     editor.addNode(add);
+    editor.addNode(square);
 
     editor.connect(n1.outputs.get('num')!, add.inputs.get('num1')!);
     editor.connect(n2.outputs.get('num')!, add.inputs.get('num2')!);
